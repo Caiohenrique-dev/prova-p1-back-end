3692e9cf2f94d105be0adb54e8ad58bdf39b9b5f
Caiohenrique-dev <pereiradasilva131@hotmail.com>
2026-09-14 20:39:07 -0300

    Update models.py

--- livros/models.py
+++ livros/models.py
@@ -8,11 +8,13 @@ class Livro(models.Model):
     isbn = models.CharField(max_length=13, unique=True)
     ano_publicacao = models.IntegerField()
     disponivel = models.BooleanField(default=True)
-
+    # Um autor pode escrever vários livros, enquanto cada livro possui um autor principal.
+    # Por isso, a relação Autor -> Livro é 1:N e a ForeignKey fica no model Livro.
  
     autor = models.ForeignKey(Autor, on_delete=models.PROTECT, related_name='livros')
 
-   
+   # Uma categoria pode agrupar vários livros e cada livro pertence a uma categoria.
+    # Essa segunda relação também é 1:N e ajuda a organizar o acervo da biblioteca.
     categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name='livros')
 
     def __str__(self):
